1bc2439e03603bc4302b5d97dfe389929f76309b
EF <66633690+Afroefras@users.noreply.github.com>
2024-02-21 17:37:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -159,8 +159,8 @@ class Game:
 if __name__ == "__main__":
     mygame = Game(
         title="Robotina",
-        tile_size=22,  # Tamaño de cada cuadrícula
-        tiles_horizontal=30,
-        tiles_vertical=20,
+        tile_size=22,           # Tamaño de cada cuadrícula
+        tiles_horizontal=30,    # Cuadrículas horizontales
+        tiles_vertical=20,      # Cuadrículas horizontales
     )
     mygame.main()
